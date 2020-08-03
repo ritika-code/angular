80b48e8d66de83a218cc85ae8636e845ee2cfc9d
riti <ritika.ritikagupta2000@gmail.com>
2020-08-03 23:54:56 +0530

    third web file

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -2,16 +2,18 @@
   'use strict';
 
   angular.module('LunchCheck',[])
-  .controller('LunchCheckController', [$'scope', $'filter', LunchCheckController]);
+  .controller('LunchCheckController',LunchCheckController);
+  LunchCheckController.$inject=['$scope','$filter'];
+function LunchCheckController($scope, $filter){
 
 
-  function LunchCheckController($scope, $filter) {
-    $scope.name="riti";
+        $scope.name="riti";
 
-    $scope.upper = function() {
-      var upCase=$filter('uppercase');
-      $scope.name= upCase($scope.name);
-    };
+        $scope.upper = function() {
+          var upCase=$filter('uppercase');
+          $scope.name= upCase($scope.name);
+        };
+    }
 
-}
+
 })();
